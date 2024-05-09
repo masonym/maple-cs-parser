c596bc942bbda0fe07ee45c3e6f10a2d0a17e209
Mason Leitch <nosamleitch@gmail.com>
2024-05-08 22:05:33 -0700

    Fixing again

--- upcoming-sales-website/src/App.js
+++ upcoming-sales-website/src/App.js
@@ -1,13 +1,12 @@
+// src/App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import ItemList from './components/ItemList.js';
+import ItemList from './components/ItemList';
 
 function App() {
   return (
     <Routes>
-      {/* other routes here */}
       <Route path="/" element={<div>Home Page</div>} />
-      {/* Route for ItemList */}
       <Route path="/ms-upcoming-sales" element={<ItemList />} />
     </Routes>
   );
