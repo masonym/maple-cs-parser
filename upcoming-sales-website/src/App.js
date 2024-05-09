9a0ca9ab69ee7c129ecf1bae816a57c4dddbad63
Mason Leitch <nosamleitch@gmail.com>
2024-05-08 21:58:08 -0700

    Fixed app.js

--- upcoming-sales-website/src/App.js
+++ upcoming-sales-website/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import ItemList from './components/ItemList';
+import ItemList from './components/ItemList.js';
 
 function App() {
   return (
