bc514f3b34b519296e5361d9a56560429f23b132
Mason Leitch <nosamleitch@gmail.com>
2024-05-09 16:49:39 -0700

    Fixed indentation bug

--- item-info-generator/data_processing.py
+++ item-info-generator/data_processing.py
@@ -27,40 +27,46 @@ def sort_xml(tree, sort_by):
 
 def package_dict_creator(qualified_tree, root_package_names, root_package_contents):
     root = qualified_tree.getroot()
+
+    # make package dir for storing data
+    # contents will be an array of SN IDs
     package_dict = {}
 
+    # get package names
     for dir_element in root.findall('imgdir'):
         item_id = dir_element.find("int[@name='ItemId']").get('value')
         if item_id.startswith("910"):
+            # for dir_element_2 in root_package_names:
             package = root_package_names.find(f"imgdir[@name='{item_id}']")
-            if package:
-                package_id = package.attrib['name']
-                package_dict[package_id] = {
-                    "name": package.find("string[@name='name']").get('value'),
-                    "description": format_description(package.find("string[@name='desc']").get('value')) if package.find("string[@name='desc']") is not None else ""
-                }
+            package_id = package.attrib['name']
+            package_dict[package_id] = {}
+            package_dict[package_id]["name"] = package.find("string[@name='name']").get('value')
+            package_dict[package_id]["description"] = package.find("string[@name='desc']").get('value').replace("#c","").replace("\\n","\n").replace("#","").replace("\\r","") if package.find("string[@name='desc']") is not None else ""
 
-                for dir_element in root_package_contents.findall('imgdir'):
-                    if package_id == dir_element.attrib['name']:
-                        package_dict[package_id]["contents"] = [int(int_element.get('value')) for int_element in dir_element.findall('.//int')]
+            for dir_element in root_package_contents.findall('imgdir'):
+                if package_id == dir_element.attrib['name']:
+                    package_dict[package_id]["contents"] = [int_element.get('value') for int_element in dir_element.findall('.//int')]
+
+
+    # return nested dictionaries
     return package_dict
 
 def package_contents_parser(package_dict, item_id, root_commodity):
-    item_ids = {item_id: {}}
-    array = package_dict[item_id].get("contents", [])
-    
-    dir_elements = {(dir_element.find('int[@name="SN"]').get('value')): dir_element for dir_element in root_commodity.findall('.//imgdir')}
-    
+    item_ids = {}
+    item_ids[item_id] = {}
+    array = package_dict[item_id].get("contents")
     for idx, sn in enumerate(array):
-        dir_element = dir_elements.get(str(sn))
-        if dir_element is not None:
-            item_id_element = dir_element.find('int[@name="ItemId"]')
-            item_count_element = dir_element.find('int[@name="Count"]')
-            if item_id_element is not None:
-                item_ids[item_id][idx] = {
-                    'itemID': item_id_element.get('value'),
-                    'count': item_count_element.get('value') if item_count_element is not None else '1'
-                }
+        for dir_element in root_commodity.findall('.//imgdir'):
+            sn_elem = dir_element.find('int[@name="SN"]')
+            if sn_elem is not None and sn_elem.get('value') == sn:
+                item_id_element = dir_element.find('int[@name="ItemId"]')
+                item_count_element = dir_element.find('int[@name="Count"]')
+                if item_id_element is not None:
+                    item_ids[item_id][idx] = {
+                        'itemID': item_id_element.get('value'),
+                        'count': item_count_element.get('value')
+                    }
+                break
     return item_ids
 
 def process_qualifying_dirs(qualified_tree, package_dict, root_commodity, root_cash, root_eqp, root_pet, search_nested_xml_for_dir_by_name):
@@ -107,8 +113,8 @@ def process_qualifying_dirs(qualified_tree, package_dict, root_commodity, root_c
                     
             item_info[item_id] = {
                 'itemID': item_id,
-                'name': package_dict[item_id]['name'],
-                'description': package_dict[item_id]['description'],
+                'name': package_dict[item_id].get('name'),
+                'description': package_dict[item_id].get('description'),
                 'count': count,
                 'price': price,
                 'discount': discount,
@@ -130,26 +136,26 @@ def process_qualifying_dirs(qualified_tree, package_dict, root_commodity, root_c
             desc_element = corresponding_dir.find("string[@name='desc']")
             description = format_description(desc_element.get('value')) if desc_element is not None else None
 
-        if item_id.startswith("500") and price == "4900":
-            pet_path = f"{PATH_PREF_ITEM}/Pet/{item_id}.img.xml"
-            pet_info_tree = ET.parse(pet_path)
-            pet_info_root = pet_info_tree.getroot()
-            life_val = pet_info_root.find(".//int[@name='life']").attrib['value']
-            period = life_val
+            if item_id.startswith("500") and price == "4900":
+                pet_path = f"{PATH_PREF_ITEM}/Pet/{item_id}.img.xml"
+                pet_info_tree = ET.parse(pet_path)
+                pet_info_root = pet_info_tree.getroot()
+                life_val = pet_info_root.find(".//int[@name='life']").attrib['value']
+                period = life_val
 
             
-        item_info[sn_id] = {
-            'itemID': item_id,
-            'name': name,
-            'count': count,
-            'description': description,
-            'price': price,
-            'discount': discount,
-            'originalPrice': original_price,
-            'termStart': term_start_f,
-            'termEnd': term_end_f,
-            'gameWorld': game_world,
-            'period': period
-        }
-    print(item_info)
+            item_info[sn_id] = {
+                'itemID': item_id,
+                'name': name,
+                'count': count,
+                'description': description,
+                'price': price,
+                'discount': discount,
+                'originalPrice': original_price,
+                'termStart': term_start_f,
+                'termEnd': term_end_f,
+                'gameWorld': game_world,
+                'period': period
+            }
+
     return item_info
