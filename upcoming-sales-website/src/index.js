202ee5a6beacf8a9390ab831fc7e32bb00f0b44e
Mason Leitch <nosamleitch@gmail.com>
2024-05-08 21:37:54 -0700

    Another routing fix

--- upcoming-sales-website/src/index.js
+++ upcoming-sales-website/src/index.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './components/Home';
+import ItemList from './components/ItemList';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function App() {
+  return (
+    <Router basename="/xyz">  // Adjust the basename according to your deployment subpath
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/ms-upcoming-sales" component={ItemList} />
+      </Switch>
+    </Router>
+  );
+}
+
+export default App;
