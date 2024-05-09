5ef29957049bf9bc9225b91333088ccc5a5ef777
Mason Leitch <nosamleitch@gmail.com>
2024-05-08 23:24:53 -0700

    Added cloudflare worker to repo

---
+++ item-info-worker/src/index.js
@@ -0,0 +1,25 @@
+addEventListener('fetch', event => {
+    event.respondWith(handleRequest(event.request))
+  })
+  
+  async function handleRequest(request) {
+    const url = new URL(request.url);
+  
+    if (url.pathname === "/api/items") {
+      // Get the data from KV
+      const data = await ITEMS_DATA.get("item_data");
+      if (data === null) {
+        return new Response("Data not found", { status: 404 });
+      }
+  
+      return new Response(data, {
+        headers: { 
+            'Content-Type': 'application/json',
+            'Access-Control-Allow-Origin': 'https://masonym.dev',
+            'Access-Control-Allow-Methods': 'GET, OPTIONS'
+        },
+      });
+    }
+  
+    return new Response("Resource not found", { status: 404 });
+  }
---
+++ item-info-worker/src/update_kv.js
@@ -0,0 +1,51 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+require('dotenv').config();
+
+// Function to read the JSON data from the file
+async function readJsonData(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(JSON.parse(data));
+    });
+  });
+}
+
+async function updateKV() {
+  // Path to the JSON file
+  const jsonFilePath = path.join(__dirname, '..', '..', 'item_info_generator', 'item_data.json');
+
+  try {
+    // Read the JSON data from the file
+    const data = await readJsonData(jsonFilePath);
+
+    // JSON data as string to be stored in KV
+    const value = JSON.stringify(data);
+
+    // The key under which to store the JSON data in KV
+    const key = "item_data";
+
+    // Example: Update KV using a direct API call
+    // Replace `ACCOUNT_ID`, `NAMESPACE_ID`, `API_TOKEN` with your actual data
+    const url = `https://api.cloudflare.com/client/v4/accounts/${process.env.CF_ACCOUNT_ID}/storage/kv/namespaces/${process.env.CF_NAMESPACE_ID}/values/${key}`;
+    
+    const response = await axios.put(url, value, {
+      headers: {
+        "Content-Type": "application/json",
+        "Authorization": `Bearer ${process.env.CF_API_TOKEN}`,
+        "Content-Length": Buffer.byteLength(value)
+      }
+    });
+
+    console.log('KV updated successfully:', response.data);
+  } catch (error) {
+    console.error('Error updating KV:', error);
+  }
+}
+
+updateKV();
