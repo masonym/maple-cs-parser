dd93cd5e2797ef831cf8c17754512b227bffb7f9
Mason Leitch <nosamleitch@gmail.com>
2024-05-08 23:01:12 -0700

    Added home page

--- upcoming-sales-website/src/App.js
+++ upcoming-sales-website/src/App.js
@@ -1,12 +1,13 @@
 // src/App.js
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import HomePage from './components/Home'
 import ItemList from './components/ItemList';
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<div>Home Page</div>} />
+      <Route path="/" element={<HomePage />} />
       <Route path="/ms-upcoming-sales" element={<ItemList />} />
     </Routes>
   );
---
+++ upcoming-sales-website/src/components/Home.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import styles from '../assets/HomePage.module.css';
+
+function App() {
+  return (
+    <div className={styles.App}>
+      <header className={styles.header}>
+        <h1>nothing to see here, really</h1>
+      </header>
+      <main className={styles.main}>
+        <section className={styles.aboutMe}>
+          <h2>About Me</h2>
+          <p>
+            Hi there, this page is pretty empty for now. I'm Mason, sometimes known as masonym or Zakum on MapleStory =)
+          </p>
+        </section>
+        <section className={styles.links}>
+          <h2>Links</h2>
+          <ul>
+            <li><a href="https://github.com/masonym">GitHub</a></li>
+            <li><a href="https://www.youtube.com/channel/UCdSDc5DnBUS6bWJgPfwfYSA">YouTube</a></li>
+          </ul>
+        </section>
+        <section className={styles.projects}>
+          <h2>Tools</h2>
+          <h4>Upcoming Cash Shop Sales</h4>
+          <a href="/ms-upcoming-sales">
+          <img src="../assets/upcoming-sales.png">
+          </img>
+          </a>
+          <p><i>A handy tool for checking the upcoming Cash Shop sales for MapleStory</i></p>
+
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
