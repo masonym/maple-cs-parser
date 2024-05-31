a404515c69a3d41c39c6e76cd66235f98a0af531
Mason Leitch <nosamleitch@gmail.com>
2024-05-30 18:44:00 -0700

    Added column layout depending on screen size

--- upcoming-sales-website/src/components/ItemCard.js
+++ upcoming-sales-website/src/components/ItemCard.js
@@ -6,30 +6,34 @@ import PackageContents from './PackageContents';
 const ItemCard = ({ item }) => {
     return (
         <li key={item.itemID} className={styles.item}>
-            <div className={styles.itemFlexContainer}>
-                <img
-                    src={`./images/${item.itemID}.png`}
-                    className={styles.itemImage}
-                    alt={item.name}
-                    onError={(e) => { e.target.style.display = 'none'; }}
-                />
-                <p>{item.name}{item.count > 1 ? ` (x${item.count})` : ''}</p>
+            <div className={styles.itemContent}>
+                <div className={styles.itemFlexContainer}>
+                    <img
+                        src={`./images/${item.itemID}.png`}
+                        className={styles.itemImage}
+                        alt={item.name}
+                        onError={(e) => { e.target.style.display = 'none'; }}
+                    />
+                    <p>{item.name}{item.count > 1 ? ` (x${item.count})` : ''}</p>
+                </div>
+                <p>{convertNewlinesToBreaks(item.description)}</p>
+                <hr />
+                <p>Duration: {item.period === '0' ? 'Permanent' : `${item.period} days`}</p>
+                <p>Price: {formatNumber(item.price)}{item.itemID.toString().startsWith('870') ? ' Mesos' : ' NX'} {item.discount == 1 ? `(was ${formatNumber(item.originalPrice)}${item.itemID.toString().startsWith('870') ? ' Mesos' : ' NX'})` : ''}</p>
+                <p>Start Date: {item.termStart}</p>
+                <p>End Date: {item.termEnd}</p>
+                <PackageContents contents={item.packageContents} />
             </div>
-            <p>{convertNewlinesToBreaks(item.description)}</p>
-            <hr />
-            <p>Duration: {item.period === '0' ? 'Permanent' : `${item.period} days`}</p>
-            <p>Price: {formatNumber(item.price)}{item.itemID.toString().startsWith('870') ? ' Mesos' : ' NX'} {item.discount == 1 ? `(was ${formatNumber(item.originalPrice)}${item.itemID.toString().startsWith('870') ? ' Mesos' : ' NX'})` : ''}</p>
-            <p>Start Date: {item.termStart}</p>
-            <p>End Date: {item.termEnd}</p>
-            <PackageContents contents={item.packageContents} />
-            {item.gameWorld.split('/').map((gameWorldId) => (
-                <img className={styles.gameWorld}
-                    key={gameWorldId}
-                    src={`./gameWorlds/${gameWorldId}.png`}
-                    alt={`Game World ${gameWorldId}`}
-                    onError={(e) => { e.target.style.display = 'none'; }} // hide the image if it doesn't exist
-                />
-            ))}
+            <div className={styles.gameWorldContainer}>
+                {item.gameWorld.split('/').map((gameWorldId) => (
+                    <img className={styles.gameWorld}
+                        key={gameWorldId}
+                        src={`./gameWorlds/${gameWorldId}.png`}
+                        alt={`Game World ${gameWorldId}`}
+                        onError={(e) => { e.target.style.display = 'none'; }} // hide the image if it doesn't exist
+                    />
+                ))}
+            </div>
         </li>
     );
 };
