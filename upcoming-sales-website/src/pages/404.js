129984a95df57b68acc14bd49d2d6f8a63fce3c2
Mason Leitch <nosamleitch@gmail.com>
2024-08-08 15:10:37 -0700

    Centered 404 page

--- upcoming-sales-website/src/pages/404.js
+++ upcoming-sales-website/src/pages/404.js
@@ -1,5 +1,5 @@
 import React from "react"
 
 export default function Custom404() {
-    return <h1>404 - you're lost!</h1>
+    return <h1 style={{display: "table-cell", verticalAlign: "middle"}}>404 - you're lost!</h1>
   }
